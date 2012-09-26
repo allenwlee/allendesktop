2e7382066eb390a4f84afb73d5853c7bbbc3aec3
Allen Lee <allenwlee130@gmail.com>
2012-09-26 12:46:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allendesktop::Application.config.secret_token = 'a3835ab4fe5279628382ffa687f9eb36c7068f3a3d28e271b74d195bd3e66ccbc48563e81d94723399c7caf31ec34d1bb711dfcc929dd8e874e4f6d7d9e64d52'
