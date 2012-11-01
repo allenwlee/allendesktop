bc13e17c57ac65b7e78e6057f21d444d188add6b
Allen Lee <allenwlee130@gmail.com>
2012-10-31 21:49:50 -0700

    rake production precompile assets

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,6 @@ module Allendesktop
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-	config.assets.precompile |= Dir["#{Rails.root}/app/assets/stylesheets/*.*"]
+
   end
 end
